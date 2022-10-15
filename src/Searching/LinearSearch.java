1472e029aebf515ef7bdd1fbb5c08dbd4440f93b
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-10-15 09:36:41 +0500

    Find All Indexes

--- src/Searching/LinearSearch.java
+++ src/Searching/LinearSearch.java
@@ -1,10 +1,14 @@
 package Searching;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,5,5};
         System.out.println(linearSearch(arr,5,0));
         System.out.println(find(arr,5,0));
+        findAllIndexes(arr,5,0);
+        System.out.println(list);
 
     }
     static int linearSearch(int[] arr, int target, int index){
@@ -22,5 +26,17 @@ public class LinearSearch {
             return false;
 
         return arr[index] == target || find(arr, target, index+1);
+    }
+
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndexes(int[] arr, int target, int index){
+        if (index == arr.length){
+            return  ;
+        }
+        if (arr[index] == target){
+          list.add(index)  ;
+        }
+        findAllIndexes(arr, target, index+1);
     }
 }
