5f48e918485d9842932669dfc205879d20d0b12e
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-10-23 15:39:40 +0500

    Quick Sort

---
+++ src/Sorting/Quick_Sort.java
@@ -0,0 +1,48 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Quick_Sort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void sort(int[] arr, int low, int high) {
+        if (low > high) {
+            return;
+        }
+        int start = low;
+        int end = high;
+
+        int mid = start + (end - start) / 2;
+        int pivot = arr[mid];
+
+        while (start <= end) {
+
+            //If Array is sorted swaping will not occure
+            while (arr[start] < pivot) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+
+            if (start <= end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        //Pivot is at its correct position
+        sort(arr, low, end);
+        sort(arr, start, high);
+    }
+
+
+}
