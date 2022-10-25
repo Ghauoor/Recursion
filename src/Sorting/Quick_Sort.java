ce67c32002fa0f35ac0922aacc3445260785ca76
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-10-25 13:33:04 +0500

    Quick Sort with adjacent element

--- src/Sorting/Quick_Sort.java
+++ src/Sorting/Quick_Sort.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class Quick_Sort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-        sort(arr, 0, arr.length - 1);
+        int[] arr = {0, 0, 2, 1, 2, 5, 7, 8, 9};
+
+        quickSort(arr, 0, arr.length - 1);
+
         System.out.println(Arrays.toString(arr));
 
     }
@@ -44,5 +46,38 @@ public class Quick_Sort {
         sort(arr, start, high);
     }
 
+    // Quick Sort Using Adjacent Element
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while (i < j) {
+            while (i < high && arr[i] <= pivot)
+                i++;
+            while (arr[j] > pivot)
+                j--;
 
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, j, low);
+        return j;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+
+        }
+    }
 }
