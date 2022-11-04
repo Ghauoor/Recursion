3c39cf1bdd47a174108b37579415ff805444e6a2
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-04 12:39:11 +0500

    Letter Combinations of a PhoneNumber

--- src/Questions/Medium/Letter_Combinations_of_a_Phone_Number.java
+++ src/Questions/Medium/Letter_Combinations_of_a_Phone_Number.java
@@ -25,4 +25,28 @@ public class Letter_Combinations_of_a_Phone_Number {
         }
     }
 
+    static ArrayList<String> padReturnArrayList(String pro, String unPro) {
+        if (unPro.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+
+            list.add(pro);
+            return list;
+
+        }
+
+        //convert '2' to 2
+        int digit = unPro.charAt(0) - '0';
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+
+            char ch = (char) ('a' + i);
+
+            list.addAll(padReturnArrayList(pro + ch, unPro.substring(1)));
+
+        }
+        return list;
+    }
+
 }
