8532de7a3241ff3a929fa01c1d16b7583fcf72e9
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-05 19:21:17 +0500

    Count The Letter Combinations of a PhoneNumber

--- src/Questions/Medium/Letter_Combinations_of_a_Phone_Number.java
+++ src/Questions/Medium/Letter_Combinations_of_a_Phone_Number.java
@@ -6,6 +6,9 @@ public class Letter_Combinations_of_a_Phone_Number {
     public static void main(String[] args) {
 
         pad("", "12");
+        System.out.println(padReturnArrayList("", "12"));
+
+        System.out.println(padCount("","12"));
     }
 
     static void pad(String pro, String unPro) {
@@ -24,6 +27,8 @@ public class Letter_Combinations_of_a_Phone_Number {
             pad(pro + ch, unPro.substring(1));
         }
     }
+
+    // return the arraylist in ans
 
     static ArrayList<String> padReturnArrayList(String pro, String unPro) {
         if (unPro.isEmpty()) {
@@ -47,6 +52,28 @@ public class Letter_Combinations_of_a_Phone_Number {
 
         }
         return list;
+    }
+
+    // Count the permutations
+
+    static int padCount(String pro, String unPro) {
+        if (unPro.isEmpty()) {
+
+            return 1;
+        }
+        
+        int count = 0;
+
+        //convert '2' to 2
+        int digit = unPro.charAt(0) - '0';
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+
+            char ch = (char) ('a' + i);
+
+            count = count + padCount(pro + ch, unPro.substring(1));
+        }
+        return count;
     }
 
 }
