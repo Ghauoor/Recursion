6c6db47d9efa9190cdf735c5cefd2e8f4e18a691
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-08 04:16:28 +0500

    Dice Number Combinations

--- src/Questions/Medium/Dice.java
+++ src/Questions/Medium/Dice.java
@@ -1,8 +1,12 @@
 package Questions.Medium;
 
+import java.util.ArrayList;
+
 public class Dice {
     public static void main(String[] args) {
         diceCombinations("", 4);
+
+        System.out.println(diceCombinationsReturnArrayList("", 4));
 
     }
     static void diceCombinations(String pro, int target){
@@ -15,5 +19,22 @@ public class Dice {
 
             diceCombinations(pro + i, target - i);
         }
+    }
+
+    static ArrayList<String> diceCombinationsReturnArrayList(String pro, int target){
+        if (target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(pro);
+            return list;
+        }
+
+        ArrayList<String > ans = new ArrayList<>();
+
+        for (int i = 1; i < 6 && i <= target; i++) {
+
+            ans.addAll(diceCombinationsReturnArrayList(pro + i, target - i));
+        }
+
+        return ans;
     }
 }
