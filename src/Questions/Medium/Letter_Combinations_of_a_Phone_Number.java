32016b64db320a7d3fa76cd6314c9174460b51e3
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-07 18:57:27 +0500

    Count The Letter Combinations of a PhoneNumber

--- src/Questions/Medium/Letter_Combinations_of_a_Phone_Number.java
+++ src/Questions/Medium/Letter_Combinations_of_a_Phone_Number.java
@@ -1,14 +1,17 @@
 package Questions.Medium;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Letter_Combinations_of_a_Phone_Number {
     public static void main(String[] args) {
 
-        pad("", "12");
-        System.out.println(padReturnArrayList("", "12"));
+        pad("", "1");
+        System.out.println(padReturnArrayList("", "1"));
 
-        System.out.println(padCount("","12"));
+        System.out.println(padCount("","1"));
+
+        System.out.println(letterCombinations("2"));
     }
 
     static void pad(String pro, String unPro) {
@@ -75,5 +78,41 @@ public class Letter_Combinations_of_a_Phone_Number {
         }
         return count;
     }
+
+    static public List<String> letterCombinations(String digits) {
+
+        ArrayList<String> ans = new ArrayList<>();
+        if (digits.length() == 0) return ans;
+        StringBuilder output = new StringBuilder();
+        int index = 0;
+        String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        letterCombinationsHelper(digits, output, index, mapping, ans);
+        return  ans;
+    }
+
+    private static void letterCombinationsHelper(String digits, StringBuilder output, int index,
+                                                 String[] mapping, ArrayList<String> ans) {
+
+        if (index >= digits.length()){
+            ans.add(output.toString());
+            return;
+        }
+
+        int num = digits.charAt(index) - '0';
+        String value = mapping[num];
+
+        for (int i = 0; i < value.length(); i++){
+
+            output.append(value.charAt(i));
+
+            letterCombinationsHelper(digits, output, index + 1, mapping, ans);
+
+            output.deleteCharAt(output.length() - 1);
+
+        }
+
+    }
+
 
 }
