25265c1a13f85a35df6139b73fa57792182220b3
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-08 11:31:10 +0500

    Backtracking Maze Path

---
+++ src/Backtracking/MatrixPath.java
@@ -0,0 +1,17 @@
+package Backtracking;
+
+ class MatrixPath {
+     public static void main(String[] args) {
+         System.out.println(count(3,6));
+     }
+   static int count (int r, int c){
+
+        if (r == 1 || c == 1)
+            return 1;
+
+        int left = count(r-1, c);
+        int right = count(r,c-1);
+
+        return left + right;
+    }
+}
