58a0fb2e548a67871bd9b5cdeae18555c6a03039
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-08 12:02:10 +0500

    Path Problems

--- src/Backtracking/MatrixPath.java
+++ src/Backtracking/MatrixPath.java
@@ -1,9 +1,12 @@
 package Backtracking;
 
- class MatrixPath {
+import java.util.ArrayList;
+
+class MatrixPath {
      public static void main(String[] args) {
-         System.out.println(count(3,6));
-         path("", 3,3);
+//         System.out.println(count(3,6));
+//         path("", 3,3);
+         System.out.println(pathReturn("", 3,2));
      }
    static int count (int r, int c){
 
@@ -31,5 +34,23 @@ package Backtracking;
          if (c > 1){
              path(p + 'R', r, c - 1);
          }
+     }
+
+     static ArrayList<String> pathReturn(String p, int r, int c) {
+         if (r == 1 && c == 1) {
+             ArrayList<String> list = new ArrayList<>();
+             list.add(p);
+             return list;
+         }
+
+         ArrayList<String> ans = new ArrayList<>();
+
+         if (r > 1) {
+             ans.addAll(pathReturn(p + 'D', r - 1, c));
+         }
+         if (c > 1) {
+             ans.addAll(pathReturn(p + 'R', r, c - 1));
+         }
+         return ans;
      }
 }
