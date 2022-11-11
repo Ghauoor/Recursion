c7a772d7fcbc08f8709653e8b45ec548460ab7e9
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-11 11:11:28 +0500

    Maze Path Problem Using Backtracking

--- src/Backtracking/AllPaths.java
+++ src/Backtracking/AllPaths.java
@@ -8,40 +8,42 @@ public class AllPaths {
                 {true, true, true}
         };
 
-        allPaths("", maze,0, 0);
+        allPath("", maze, 0, 0);
 
     }
 
-    //ToDo: Solve Stackoverflow
-
-    static void allPaths(String p, boolean[][] maze, int r, int c) {
-
+    static void allPath(String p, boolean[][] maze, int r, int c) {
         if (r == maze.length - 1 && c == maze[0].length - 1) {
-
             System.out.println(p);
             return;
         }
 
-
-        if (!maze[r][c])
+        if (!maze[r][c]) {
             return;
+        }
+
+        // I am considering this block in my path
+        maze[r][c] = false;
 
         if (r < maze.length - 1) {
-            allPaths(p + 'D',maze ,r + 1, c );
+            allPath(p + 'D', maze, r + 1, c);
         }
 
         if (c < maze[0].length - 1) {
-            allPaths(p + 'R', maze,r, c + 1);
+            allPath(p + 'R', maze, r, c + 1);
         }
 
         if (r > 0) {
-            allPaths(p + 'U', maze,r - 1, c);
+            allPath(p + 'U', maze, r - 1, c);
         }
 
         if (c > 0) {
-            allPaths(p + 'L', maze,r - 1, c - 1);
+            allPath(p + 'L', maze, r, c - 1);
         }
 
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
     }
 
 }
