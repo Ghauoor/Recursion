09f98755daedf52e1b7526547ff1bd9e9014bf35
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-15 12:56:24 +0500

    N-Knights Problem

--- src/Backtracking/N_Knight.java
+++ src/Backtracking/N_Knight.java
@@ -60,7 +60,6 @@ public class N_Knight {
         return true;
     }
 
-    // do not repeat yourself, hence created this function
     static boolean isValid(boolean[][] board, int row, int col) {
         if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
             return true;
