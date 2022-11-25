cb6b6c24654247063c64516c81ae09b2e2a6f3ad
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-11-25 19:31:37 +0500

    SUD0KO-SOLVER Problem

--- src/Backtracking/Sudoku_Solver.java
+++ src/Backtracking/Sudoku_Solver.java
@@ -41,12 +41,12 @@ public class Sudoku_Solver {
                 }
             }
             // if you found some empty element in row, then break
-            if (emptyLeft == false) {
+            if (!emptyLeft) {
                 break;
             }
         }
 
-        if (emptyLeft == true) {
+        if (emptyLeft) {
             return true;
             // soduko is solved
         }
@@ -107,4 +107,78 @@ public class Sudoku_Solver {
         }
         return true;
     }
+
+
+    //LeetCode Solution
+
+    public boolean isSafeForBoard(char[][] board, int row, int col, int number) {
+        //column
+        for (char[] chars : board) {
+            if (chars[col] == (char) (number + '0')) {
+                return false;
+            }
+        }
+
+        //row
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == (char) (number + '0')) {
+                return false;
+            }
+        }
+
+        //grid
+        int sr = 3 * (row / 3);
+        int sc = 3 * (col / 3);
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean helper(char[][] board, int row, int col) {
+        if (row == board.length) {
+            return true;
+        }
+
+        int nrow = 0;
+        int ncol = 0;
+
+        if (col == board.length - 1) {
+            nrow = row + 1;
+            ncol = 0;
+        } else {
+            nrow = row;
+            ncol = col + 1;
+        }
+
+        if (board[row][col] != '.') {
+            return helper(board, nrow, ncol);
+        } else {
+
+            //fill the place
+            for (int i = 1; i <= 9; i++) {
+                if (isSafeForBoard(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (helper(board, nrow, ncol))
+                        return true;
+                    else
+                        board[row][col] = '.';
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
 }
+
+
